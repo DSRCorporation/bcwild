c96720af81a6fa0fe2a6335859e0cef5717eb46a
Andrew Nadraliev <andrew.nadraliev@dsr-corporation.com>
2023-09-13 13:20:31 +0100

    [BCGOVWILD-83] Implemented a utility to get current gps location. Added to Aerial telemetry

--- app/BCWildlife/screens/Location.js
+++ app/BCWildlife/screens/Location.js
@@ -1,69 +1,99 @@
-import React, { useEffect, useState } from 'react';
-import { Alert, PermissionsAndroid } from 'react-native';
-import Geolocation from '@react-native-community/geolocation';
-import { fromLonLat } from 'proj4';
+import React, {useCallback} from 'react';
+import { Alert, Linking, PermissionsAndroid, Platform } from "react-native";
+import {fromLonLat} from 'proj4';
+import IntentLauncher from 'react-native-intent-launcher';
+import DeviceInfo from 'react-native-device-info';
+import Geolocation from 'react-native-geolocation-service';
 
-const Location = () => {
-  const [locationPermission, setLocationPermission] = useState(null);
+export const useLocation = () => {
+  const packageId = DeviceInfo.getBundleId();
 
-  useEffect(() => {
-    requestLocationPermission();
+  const requestLocationPermissionAndroid = useCallback(async () => {
+    const result = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Location permission required',
+        message:
+          'This app needs to access your location to get current position',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    return result === PermissionsAndroid.RESULTS.GRANTED;
   }, []);
 
-  const requestLocationPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'Location permission required',
-          message:
-            'This app needs to access your location to get current position',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        setLocationPermission(true);
-        getLocation();
-      } else {
-        setLocationPermission(false);
-        console.log('Location permission denied');
-      }
-    } catch (err) {
-     // console.warn(err);
+  const requestLocationPermissioniOS = useCallback(async () => {
+    return (await Geolocation.requestAuthorization('whenInUse')) === 'granted';
+  }, []);
+
+  const requestLocationPermission = useCallback(async () => {
+    if (Platform.OS === 'android') {
+      return await requestLocationPermissionAndroid();
+    } else {
+      return await requestLocationPermissioniOS();
     }
-  };
+  }, [requestLocationPermissioniOS, requestLocationPermissionAndroid]);
 
-  const getLocation = () => {
-    Geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-        const utmCoordinates = fromLonLat([longitude, latitude]);
-        const { northing, easting } = getUTMCoordinates(utmCoordinates);
-        console.log(`Northing: ${northing}, Easting: ${easting}`);
-      },
-      error => console.log(error),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-  };
+  const getLocation = useCallback(() => {
+    return new Promise((resolve, reject) => {
+      Geolocation.getCurrentPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          const utmCoordinates = [latitude, longitude];
+          const coords = getUTMCoordinates(utmCoordinates);
+          console.log(
+            `Northing: ${coords.northing}, Easting: ${coords.easting}`,
+          );
+          resolve(coords);
+        },
+        error => {
+          console.warn(`Error getting current location ${error.message}`);
+          reject(error);
+        },
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+      );
+    });
+  }, []);
 
-  const getUTMCoordinates = (utmCoordinates) => {
+  const getUTMCoordinates = utmCoordinates => {
     const [easting, northing] = utmCoordinates;
-    return { easting, northing };
+    return {easting, northing};
   };
 
-  if (locationPermission === null) {
-    return null;
-  } else if (locationPermission === false) {
-    return (
-      <View>
-        <Text>Location permission denied</Text>
-      </View>
-    );
-  } else {
-    return null;
-  }
-};
+  const openPermissionSettings = useCallback(() => {
+    if (Platform.OS === 'ios') {
+      Linking.openURL('app-settings:');
+    } else {
+      IntentLauncher.startActivity({
+        action: 'android.settings.APPLICATION_DETAILS_SETTINGS',
+        data: 'package:' + packageId,
+      });
+    }
+  }, [packageId]);
 
-export default Location;
+  const showPermissionRequiredAlert = useCallback(() => {
+    Alert.alert(
+      'Location permission',
+      'To get current location please go to settings to enable Location permission.',
+      [
+        {
+          text: 'Open settings',
+          onPress: () => openPermissionSettings(),
+        },
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+      ],
+    );
+  }, [openPermissionSettings]);
+
+  return {
+    requestLocationPermission,
+    getLocation,
+    openPermissionSettings,
+    showPermissionRequiredAlert,
+  };
+};
